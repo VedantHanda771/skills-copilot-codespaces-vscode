0091c175a14ae22d0156a6e57b1bcbec51434955
Vedant Handa <122337658+VedantHanda771@users.noreply.github.com>
2024-10-04 15:30:03 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,7 @@
+function skillsMember(){
+    console.log("Skills");
+    let skills = ["HTML", "CSS", "JS", "React", "Node", "Express", "MongoDB", "Python", "C++"];
+    for (let i = 0; i < skills.length; i++){
+        console.log(skills[i]);
+    }
+}
